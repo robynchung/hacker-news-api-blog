548581dab5212adada3ab1d11dea61f89e62abb3
Ari <aeri@hesper.io>
2020-08-06 09:38:21 -0400

    remove optional chanining

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,27 +3,31 @@ import moment from "moment";
 
 import { ContentContainer, Image, ReadMore, TitleWrapper, TopicWrapper } from "../layouts/Story";
 const Story = ({ story }) => {
-  return (
-    <>
-      <Image backgroundImage={story.metaData["og:image"] || story["twitter:image"]} />
+  if (story) {
+    return (
+      <>
+        <Image backgroundImage={story.metaData["og:image"] || story["twitter:image"]} />
 
-      <ContentContainer>
-        <TopicWrapper>{story?.type}</TopicWrapper>
-        <div>{moment.unix(story?.time).format("YYYY-MM-DD hh:mm")}</div>
-        <TitleWrapper>
-          <a href={story?.url} target="_blank">
-            {story?.title}
-          </a>
-        </TitleWrapper>
+        <ContentContainer>
+          <TopicWrapper>{story.type}</TopicWrapper>
+          <div>{moment.unix(story.time).format("YYYY-MM-DD hh:mm")}</div>
+          <TitleWrapper>
+            <a href={story.url} target="_blank">
+              {story.title}
+            </a>
+          </TitleWrapper>
 
-        <div>
-          <ReadMore href={story?.url} target="_blank">
-            read this article
-          </ReadMore>
-        </div>
-      </ContentContainer>
-    </>
-  );
+          <div>
+            <ReadMore href={story.url} target="_blank">
+              read this article
+            </ReadMore>
+          </div>
+        </ContentContainer>
+      </>
+    );
+  }
+
+  return null;
 };
 
 export default Story;
