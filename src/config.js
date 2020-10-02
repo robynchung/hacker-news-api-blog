59601ac15bb87b4152444fcb0affd4268bf28aa6
Aeri <rjungdesign@gmail.com>
2020-10-02 19:58:26 -0400

    add query

--- src/config.js
+++ src/config.js
@@ -1,11 +1,24 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { RestLink } from "apollo-link-rest";
 
+import { api } from "./constants";
+
 const client = new ApolloClient({
-  link: new RestLink({ uri: "http://localhost:3000/" }),
+  link: new RestLink({ uri: api.hackerNews }),
   cache: new InMemoryCache()
 });
 
-console.log(client.link);
+const query = gql`
+  query luke {
+    person @rest(type: "Person", path: "people/1/") {
+      name
+    }
+  }
+`;
+
+client.query({ query }).then(response => {
+  console.log(response);
+  // console.log(response.data.name);
+});
 
 export default { client };
--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,8 @@ module.exports = {
   numOfBlog: 30,
   inputType: {
     submit: "submit"
+  },
+  api: {
+    hackerNews: "https://hacker-news.firebaseio.com/v0/newstories.json"
   }
 };
